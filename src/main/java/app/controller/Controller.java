521813323b3e3d8b67a783c4af03e05ae6957741
krasnovss91 <krasnovstanislav91@gmail.com>
2022-09-03 19:31:39 +0400

    Controller_1

--- src/main/java/app/controller/Controller.java
+++ src/main/java/app/controller/Controller.java
@@ -1,6 +1,8 @@
 package app.controller;
 
 import app.entity.Triangle;
+import app.service.TriangleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,14 @@ import java.math.BigDecimal;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    TriangleService triangleService;
+
     @GetMapping("/triangle")
-    public Triangle getTriangle(@RequestParam BigDecimal a, @RequestParam BigDecimal b, @RequestParam BigDecimal c) {
-        return new Triangle(a, b, c);
+    public double getTriangle(@RequestParam BigDecimal a, @RequestParam BigDecimal b, @RequestParam BigDecimal c) {
+        Triangle triangle = new Triangle(a,b,c);
+
+        return  triangleService.getArea(triangle);
     }
 }
--- src/main/java/app/service/TriangleServiceImpl.java
+++ src/main/java/app/service/TriangleServiceImpl.java
@@ -1,8 +1,6 @@
 package app.service;
 
-import app.controller.Controller;
 import app.entity.Triangle;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -10,20 +8,16 @@ import java.math.BigDecimal;
 @Service
 public class TriangleServiceImpl implements TriangleService {
 
-    @Autowired
-    Controller controller;
-
     @Override
     public BigDecimal getPerimetre(BigDecimal a, BigDecimal b, BigDecimal c) {
-         //   public BigDecimal getPeimetre(){
-       // controller.getTriangle()
+
         BigDecimal d = new BigDecimal("2.0");
         return (a.add(b).add(c)).divide(d);
     }
 
     @Override
     public double getArea(Triangle triangle) {
-        BigDecimal p = getPerimetre(triangle.getA(), triangle.getB(), triangle.getC());//получить результат с контроллера
+        BigDecimal p = getPerimetre(triangle.getA(), triangle.getB(), triangle.getC());
 
         BigDecimal a = p.subtract(triangle.getA());
         BigDecimal b = p.subtract(triangle.getB());
